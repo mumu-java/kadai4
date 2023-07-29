1d5472fdcc72b805058fec5bff0ee82b8345f4a6
mumu-java <140181017+mumu-java@users.noreply.github.com>
2023-07-29 18:00:11 +0900

    Add files via upload

---
+++ StreamCode.java
@@ -0,0 +1,17 @@
+
+
+import java.util.List;
+
+public class StreamCode {
+
+public static void main(String[] args) {
+    List<Integer>intList=List.of(1,2,3,4,5,6,7,8,9,10);
+    intList.stream().
+        filter(i->i%2==0).
+        forEach(System.out::println);
+    }
+}
+
+
+
+
